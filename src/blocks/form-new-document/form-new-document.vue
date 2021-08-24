3a5a17d74d0f62a32a1ba110e971a2c765178131
sprodanov <sprodanov@htmlbox.net>
2021-08-24 10:41:18 +0300

    New document - modify form

---
+++ src/blocks/form-new-document/form-new-document.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="form form--lg form--new-document">
+    <form action="?" method="post" @submit.prevent="onSubmit">
+      <div class="form__head form__head--center">
+        <h2>Upload Your Research Text</h2>
+        
+        <p>DataSeer will process the document and highlight passages which may need links to a dataset</p>
+      </div><!-- /.form__head -->
+
+      <div class="form__statuses" v-if="errors">
+        <div class="form__status form__status--error">
+          <p>{{errorMessage}}</p>
+        </div><!-- /.form__status -->
+      </div><!-- /.form__statuses -->
+      
+      <div class="form__body">
+        <FormFile 
+          :file="primaryFile"
+          accept=".docx, .pdf"
+          name="primaryFile"
+          :error="errors"
+          :text="getFileName('primaryFile')"
+          @onChange="onFieldChange"
+        >
+          <template #label>
+            Select Your Primary File
+          </template>
+
+          <template #helptext>
+            Supported formats: <strong>PDF, docx</strong>
+          </template>
+        </FormFile>  
+
+        <FormFile 
+          :file="additionalFiles"
+          name="additionalFiles"
+          multiple="multiple"
+          :text="getFileName('additionalFiles')"
+          @onChange="onFieldChange"
+        >
+          <template #label>
+            Select Additional Support Files <em>Optional</em>
+          </template>
+
+          <template #helptext>
+            All file formats supported
+          </template>
+        </FormFile>  
+        
+        <div class="form__row">
+          <div class="checkboxes checkboxes--center">
+            <ul>
+              <FormCheckbox
+                name="checkbox"
+                @onChange="onCheckboxChange"
+              />
+            </ul>
+          </div><!-- /.checkboxes -->
+        </div><!-- /.form__row -->
+      </div><!-- /.form__body -->
+      
+      <div class="form__actions">
+        <ul>
+          <li>
+            <Button type="submit">Upload Documents</Button>
+          </li>
+          
+          <li>
+            <Button alt>Cancel</Button>
+          </li>
+        </ul>
+      </div><!-- /.form__actions -->
+    </form>
+  </div><!-- /.form -->
+</template>
+
+<script>
+  import Button from '@/components/button/button';
+  import FormFile from '@/components/form-file/form-file';
+  import FormCheckbox from '@/components/form-checkbox/form-checkbox';
+
+  export default {
+    name: 'FormNewDocument',
+    data: function() {
+      return {
+        primaryFile: '',
+        additionalFiles: '',
+        checkbox: false,
+        errorMessage: '',
+        errors: false
+      }
+    },
+
+    components: {
+      Button,
+      FormFile,
+      FormCheckbox
+    },
+
+    methods: {
+      getFileName(name) {
+        if(this[name]) {
+          return this[name].split('\\').pop()
+        } else {
+          return '';
+        }
+      },
+      onFieldChange(event) {
+        const name = event.target.name;
+        this[name] = event.target.value;
+      },
+      onCheckboxChange(event) {
+        const name = event.target.name;
+        this[name] = event.target.checked;
+      },
+      onSubmit() {
+        if (!this.primaryFile) {
+          this.errors = true;
+          this.errorMessage = 'Primary file is empty'
+        }
+      }
+    }
+  }
+</script>
---
+++ src/components/form-checkbox/form-checkbox.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <div class="checkbox" :class="{'has-error': error}">
+      <input type="checkbox" :name="name" :id="name" @change="handleChange">
+
+      <label :for="name">This is a new version of an article DataSeer has already assessed</label>
+    </div><!-- /.checkbox -->
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'FormCheckbox',
+  props: ['error', 'name'],
+
+  methods: {
+    handleChange(event){
+      this.$emit('onChange', event);
+    }
+  }
+}
+</script>
